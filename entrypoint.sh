29044cf368cb933679beb622e727529dcfd1db09
Marcelo Prado <marceloterreiroprado@gmail.com>
2020-04-03 18:38:47 -0300

    changes diff algorithm to compare HEADs

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 SOURCE=${SOURCE:-.}
 
-cd ${GITHUB_WORKSPACE}/${SOURCE}
+cd ${GITHUB_WORKSPACE:-.}/${SOURCE}
 
 # This script returns `true` if the paths passed as
 # arguments were changed in the last commit.
@@ -23,18 +23,14 @@ if [ -z "$PATHS_TO_SEARCH" ]; then
     exit 1
 fi
 
-# 3. Get the latest commit
-LATEST_COMMIT=$(git rev-parse HEAD)
-
-# 4. Get the latest commit in the searched paths
-LATEST_COMMIT_IN_PATH=$(git log -1 --format=format:%H --full-diff $PATHS_TO_SEARCH)
-
-if [ $LATEST_COMMIT != $LATEST_COMMIT_IN_PATH ]; then
-    echo "Code in the following paths has not changed:"
-    echo $PATHS_TO_SEARCH
+# 3. Compares paths from latest HEAD with previous one. 
+# --quiet: exits with 1 if there were differences (https://git-scm.com/docs/git-diff)
+if git diff --quiet HEAD~1 HEAD -- $PATHS_TO_SEARCH; then
+    echo "Code in the following paths hasn't changed: " $PATHS_TO_SEARCH
     echo ::set-output name=changed::false
     exit 0
+else
+    echo "Code in the following paths changed: " $PATHS_TO_SEARCH
+    echo ::set-output name=changed::true
+    exit 0
 fi
-
-echo ::set-output name=changed::true
-exit 0

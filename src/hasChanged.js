6c053241d45ac14c92d39e499189e41818ede09c
Marcelo Prado <marceloterreiroprado@gmail.com>
2020-08-22 17:14:04 -0300

    wip/testing

--- src/hasChanged.js
+++ src/hasChanged.js
@@ -3,8 +3,6 @@ const exec = require('@actions/exec')
 async function main(pathsToSearch = '') {
   throwsForInvalidPaths(pathsToSearch)
 
-  await changeDirectory()
-
   return hasChanged(pathsToSearch)
 }
 
@@ -13,14 +11,18 @@ function throwsForInvalidPaths(pathsToSearch) {
   throw new Error('pathsToSearch needs to be a string')
 }
 
-async function changeDirectory() {
+function getCWD() {
   const { GITHUB_WORKSPACE = '.', SOURCE = '.' } = process.env
-  await exec.exec(`cd ${GITHUB_WORKSPACE}/${SOURCE}`)
+  return `${GITHUB_WORKSPACE}/${SOURCE}`
 }
 
 async function hasChanged(pathsToSearch) {
   //  --quiet: exits with 1 if there were differences (https://git-scm.com/docs/git-diff)
-  const exitCode = await exec.exec('git diff', ['--quiet', 'HEAD~1 HEAD', '--', pathsToSearch], { ignoreReturnCode: true, silent: true })
+  const exitCode = await exec.exec('git diff', ['--quiet', 'HEAD~1 HEAD', '--', pathsToSearch], {
+    ignoreReturnCode: true,
+    silent: true,
+    cwd: getCWD()
+  })
 
   const pathsChanged = exitCode === 1
 

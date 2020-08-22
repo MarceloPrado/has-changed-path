fbd5616556509adf30c2260092a7eb780fd280f0
Marcelo Prado <marceloterreiroprado@gmail.com>
2020-08-22 17:32:55 -0300

    wip/testing

--- dist/index.js
+++ dist/index.js
@@ -1011,7 +1011,7 @@ async function run() {
     const paths = core.getInput('paths', { required: true });
     const changed = await hasChanged(paths)
 
-
+    core.info('Action output: ', changed)
     if (changed) {
       core.info(`Code in the following paths changed: ${paths}`)
     } else {

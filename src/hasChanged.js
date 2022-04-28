c96e4771188c03eda9407ea20421275f128337a5
Philip Niedertscheider <dev@philpri.me>
2022-04-28 19:09:41 +0200

    Added support for detecting pull requests

--- src/hasChanged.js
+++ src/hasChanged.js
@@ -1,41 +1,82 @@
-const exec = require('@actions/exec')
+const exec = require("@actions/exec");
+const core = require("@actions/core");
 
-async function main(pathsToSearch = '') {
-  throwsForInvalidPaths(pathsToSearch)
+async function main(pathsToSearch = "") {
+  throwsForInvalidPaths(pathsToSearch);
 
-  return hasChanged(pathsToSearch)
+  return hasChanged(pathsToSearch);
 }
 
 function throwsForInvalidPaths(pathsToSearch) {
-  if (pathsToSearch && typeof pathsToSearch === 'string') return
-  throw new Error('pathsToSearch needs to be a string')
+  if (pathsToSearch && typeof pathsToSearch === "string") return;
+  throw new Error("pathsToSearch needs to be a string");
 }
 
 function getCWD() {
-  const { GITHUB_WORKSPACE = '.', SOURCE = '.' } = process.env
-  return `${GITHUB_WORKSPACE}/${SOURCE}`
+  const { GITHUB_WORKSPACE = ".", SOURCE = "." } = process.env;
+  return `${GITHUB_WORKSPACE}/${SOURCE}`;
 }
 
-async function hasChanged(pathsToSearch) {
-  const paths = pathsToSearch.split(' ')
+async function hasChanged(pathsToSearch, targetBranch) {
+  const paths = pathsToSearch.split(" ");
+  const cwd = getCWD();
 
+  let targetHash;
+  if (targetBranch) {
+    core.info(`Comparing against destination branch: ${targetBranch}`);
+    targetHash = targetBranch;
+  } else {
+    // Print information about current commit
+    core.info(`Current working directory: ${cwd}`);
+    const currentCommit = await exec.getExecOutput(
+      "git",
+      ["--no-pager", "log"],
+      {
+        ignoreReturnCode: true,
+        silent: false,
+        cwd: cwd,
+      }
+    );
+    if (currentCommit.exitCode != 0) {
+      core.info(
+        `Failed to detect previous commit, exit code ${currentCommit.exitCode}`
+      );
+    }
+    // Get hash of previous commit
+    const previousCommitHash = await exec.getExecOutput(
+      "git",
+      ["log", "-1", "--format=%H", "HEAD~1"],
+      {
+        ignoreReturnCode: true,
+        silent: true,
+        cwd: cwd,
+      }
+    );
+    if (previousCommitHash.exitCode != 0) {
+      throw new Error(
+        `Failed to detect previous commit, exit code ${previousCommitHash.exitCode}`
+      );
+    }
+    core.info(
+      `Comparing against previous commit:\n${previousCommitHash.stdout}`
+    );
+    targetHash = previousCommitHash.stdout.trim();
+  }
   //  --quiet: exits with 1 if there were differences (https://git-scm.com/docs/git-diff)
-  const exitCode = await exec.exec('git', [
-    'diff',
-    '--quiet',
-    'HEAD~1',
-    'HEAD',
-    '--',
-    ...paths,
-  ], {
-    ignoreReturnCode: true,
-    silent: false,
-    cwd: getCWD()
-  })
-
-  const pathsChanged = exitCode === 1
-
-  return pathsChanged
+  const exitCode = await exec.getExecOutput(
+    "git",
+    ["diff", "--quiet", "HEAD", targetHash, "--", ...paths],
+    {
+      ignoreReturnCode: true,
+      silent: false,
+      cwd: cwd,
+    }
+  );
+  // If there is output in the stderr, something went wrong
+  if (exitCode.stderr.length > 0) {
+    throw new Error(`git diff had an failed. Output:\n${exitCode.stderr}`);
+  }
+  return exitCode === 1;
 }
 
-module.exports = main
+module.exports = main;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
-
-const core = require('@actions/core');
-
-const hasChanged = require('./hasChanged');
+const core = require("@actions/core");
+const hasChanged = require("./has-changed");
+const github = require("@actions/github");
 
 async function run() {
   try {
-    const paths = core.getInput('paths', { required: true });
-    const changed = await hasChanged(paths)
+    const paths = core.getInput("paths", { required: true });
+    const isPullRequest = github.context.eventName == "pull_request";
+    let targetBranch;
+    if (isPullRequest) {
+      targetBranch = github.context.targetBranch;
+    }
+    const changed = await hasChanged(paths, targetBranch);
 
     if (changed) {
-      core.info(`Code in the following paths changed: ${paths}`)
+      core.info(`Code in the following paths changed: ${paths}`);
     } else {
-      core.info(`Code in the following paths hasn't changed: ${paths}`)
+      core.info(`Code in the following paths hasn't changed: ${paths}`);
     }
 
-    core.setOutput('changed', changed)
+    core.setOutput("changed", changed);
   } catch (error) {
     core.setFailed(error.message);
   }
